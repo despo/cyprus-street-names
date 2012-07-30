9f0950e5d65f6bcd8a39bb3d109a8930a6403ece
Despo Pentara <despo@despo.me>
2012-07-31 00:54:47 +0300

    refactoring

--- lib/street_name_parser.rb
+++ lib/street_name_parser.rb
@@ -5,12 +5,14 @@ module StreetNames
 
     attr_reader :streets
 
-    def load_cities locale="en"
-      Spreadsheet.open("./data/cyprus_postcode_dir_#{locale}.xls") do |spreadsheet|
-        @streets ||= CITIES.each_value.map do |city|
-          spreadsheet.worksheets[city].map {|parameters| Street.new parameters }
-        end.inject(:+)
-      end
+    def initialize locale="en"
+      @locale = locale
+    end
+
+    def load_cities
+      @streets ||= CITIES.each_value.map do |city|
+        spreadsheet.worksheets[city].map {|parameters| Street.new parameters }
+      end.inject(:+)
     end
 
     def find_by_postcode postcode
@@ -19,6 +21,11 @@ module StreetNames
 
     def find_by_street_name street_name
       @streets.select { |street| street.name == street_name }
+    end
+
+    private
+    def spreadsheet
+      @spreadsheet ||= Spreadsheet.open("./data/cyprus_postcode_dir_#{@locale}.xls")
     end
 
   end
