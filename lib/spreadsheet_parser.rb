23c8428ead00526110e7c766e9895c506b154903
Despo Pentara <despo@despo.me>
2012-07-30 21:45:38 +0300

    shorten block

--- lib/spreadsheet_parser.rb
+++ lib/spreadsheet_parser.rb
@@ -4,9 +4,7 @@ class SpreadsheetParser
   def self.load locale="en"
     Spreadsheet.open("./data/cyprus_postcode_dir_#{locale}.xls") do |spreadsheet|
       worksheet = spreadsheet.worksheets[8]
-      streets = worksheet.map do |parameters|
-        street = Street.new parameters
-      end
+      worksheet.map { |parameters| Street.new parameters }
     end
   end
 
