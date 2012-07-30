1a52d59a2875ab9f5874c2683ae25192457bd857
Despo Pentara <despo@despo.me>
2012-07-31 00:46:08 +0300

    rename 'address' to street

--- spec/street_name_parser_spec.rb
+++ spec/street_name_parser_spec.rb
@@ -7,6 +7,7 @@ describe StreetNames::Parser do
 
     it 'load streets from spreadsheet' do
       street = { :name => "Agias Marinas", :postcode => 8041, :area => "Pafos"}
+
       street_parser.streets.map(&:to_json).should include street.to_json
     end
 
@@ -18,9 +19,9 @@ describe StreetNames::Parser do
       end
 
       it 'find street  by street name' do
-        address = { :name => "Dimosthenous Georgiou", :postcode => 8020, :area => "Pafos"}
+        street = { :name => "Dimosthenous Georgiou", :postcode => 8020, :area => "Pafos"}
 
-        street_parser.find_by_street_name("Dimosthenous Georgiou").map(&:to_json).should include address.to_json
+        street_parser.find_by_street_name("Dimosthenous Georgiou").map(&:to_json).should include street.to_json
       end
     end
   end
